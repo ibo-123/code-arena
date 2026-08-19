b59b19db057692a147956b5ff92cf4a26e75b8fa
Ibrahim Kedir <ibrahimkediramdela2211@gmail.com>
2026-08-19 22:36:18 +0300

    our code-arena website is initalize

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-[#050507] text-white">
+      <h1 className="text-4xl font-bold">Code Arena 2026</h1>
+    </div>
+  );
+}
+
+export default App;
